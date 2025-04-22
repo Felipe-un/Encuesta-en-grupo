b309bcb39882b547564770faa1b97eb3abf0172a
JulianS <jsantanag@unal.edu.co>
2025-04-22 15:22:21 -0500

    Primer trabajo en grupo

---
+++ src/encuesta/Encuesta.java
@@ -0,0 +1,14 @@
+package encuesta;
+/**
+ *
+ * @author Estudiante
+ */
+public class Encuesta {
+
+    public static void main(String[] args) {
+        
+        //Realizar una encuesta y convertir los datos
+        System.out.println("Bienvenid@. Por favor, diligencie la siguiente encuesta:");
+    }
+    
+}
