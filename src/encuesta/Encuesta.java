4c2bdf42482df17533edd7d72c082c0da5bf4a00
Sergiox098 <segomezco@unal.edu.co>
2025-04-22 15:36:56 -0500

    Creación del scanner

--- src/encuesta/Encuesta.java
+++ src/encuesta/Encuesta.java
@@ -3,11 +3,13 @@ package encuesta;
  *
  * @author Estudiante
  */
+import java.util.Scanner;
 public class Encuesta {
 
     public static void main(String[] args) {
         
         //Realizar una encuesta y convertir los datos
+        Scanner ConsoleInput = new Scanner(System.in);
         System.out.println("Bienvenid@. Por favor, diligencie la siguiente encuesta:");
     }
     
