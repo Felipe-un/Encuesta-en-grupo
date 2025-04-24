fa86ebc631e0e6cdca4989d44c3fb00ac33dfafd
JulianS <jsantanag@unal.edu.co>
2025-04-24 14:32:56 -0500

    1. Primer mensaje

--- src/encuesta/Encuesta.java
+++ src/encuesta/Encuesta.java
@@ -8,6 +8,8 @@ public class Encuesta {
 
     public static void main(String[] args) {
         
+        // Hola
+        
         //Realizar una encuesta y convertir los datos
         Scanner ConsoleInput = new Scanner(System.in);
         System.out.println("Bienvenid@. Por favor, diligencie la siguiente encuesta:");
